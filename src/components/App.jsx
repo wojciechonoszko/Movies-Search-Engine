927f7219663b8779cab5f58c38a60f18b9e94f5a
Wojciech <wojciechonoszko@gmail.com>
2022-06-27 19:20:08 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
+import Spinner from 'components/Spinner/Spinner';
+import Navigations from './Layout/Layout';
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const MovieDetail = lazy(() => import('../pages/MoviesPage/MovieDetail'));
+const Cast = lazy(() => import('../pages/Cast/Cast'));
+const Reviews = lazy(() => import('../pages/Reviews/Reviews'));
+const Movies = lazy(() => import('../pages/Movies/SearchMovies'));
+
+
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      
+
+    </>
   );
 };
