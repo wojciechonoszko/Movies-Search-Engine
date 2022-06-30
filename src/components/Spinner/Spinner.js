6a5c8e820f57a04da35dbfd06936a9cb1e03b54d
Wojciech <wojciechonoszko@gmail.com>
2022-06-30 13:22:10 +0200

    Update Spinner.js

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,15 +2,15 @@ import { BallTriangle } from 'react-loader-spinner';
 import { LoaderContainer } from './SpinnerStyles';
 
 export default function Spinner() {
-    return (
-        <LoaderContainer>
-            <BallTriangle
-            type="Hearts"
-            color="#ec7e89"
-            height={120}
-            width={120}
-            timeout={8000}
-            />
-        </LoaderContainer>
-    );
+  return (
+    <LoaderContainer>
+      <BallTriangle
+        type="BallTriangle"
+        color="darkgrey"
+        height={120}
+        width={120}
+        timeout={8000}
+      />
+    </LoaderContainer>
+  );
 }
